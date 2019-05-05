6bbba159ee38bc66ee6e496235575536a3e51116
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-05 02:28:31 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,7 +5,7 @@ def loop_iterator(number_of_times)
   number_of_times = 0
   loop do 
     number_of_times += 1
-    puts "Welcome to Flatiron School's Web Development Course!"
+    puts phrase
     if number_of_times >= 7
       break
     end
